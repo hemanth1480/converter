c4d880afa60d6aebb73810863df081d7d6da2ff4
hemanth1480 <hemanth.m1480@gmail.com>
2022-06-19 18:42:27 +0530

    hk

--- pythonscripts/doctopdf/index.py
+++ pythonscripts/doctopdf/index.py
@@ -2,8 +2,8 @@ from genericpath import exists
 from itertools import count
 import sys
 import os
-import comtypes
-import comtypes.client
+import win32com.client
+# import comtypes.client
 import shutil
 
 wdFormatPDF = 17
@@ -15,7 +15,7 @@ count =0
 for doc in docs:
     in_file = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  + "/files/" + sys.argv[1] + "/" + doc)
     out_file = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  + "/output-files/doctopdf/" + sys.argv[1] + "/" + doc.split(".")[0] + ".pdf")
-    word = comtypes.client.CreateObject('Word.Application')
+    word = win32com.client.Dispatch('Word.Application')
     doc = word.Documents.Open(in_file)
     doc.SaveAs(out_file, FileFormat=wdFormatPDF)
     count +=1
